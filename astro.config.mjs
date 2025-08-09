1dea46a4e359c473070ea0ed729400af19f8f8fc
hyperoot <rajesh@hyperoot.dev>
2025-08-09 18:01:22 +0000

    fix: update file permissions for astro.config.mjs and clean up formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,24 @@
-// @ts-check
-
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha",
+      },
     },
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: "catppuccin-latte",
-                dark: "catppuccin-mocha",
-            },
-        },
-    },
-
-    integrations: [
-        icon({
-            iconDir: "src/assets/icons",
-        }),
-        mdx(),
-    ],
+  },
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+    mdx(),
+  ],
 });
