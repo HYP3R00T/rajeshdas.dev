f97313e47f4d4e24af601fd0a01278ad788f181a
Hyperoot <rajesh@hyperoot.dev>
2025-09-03 14:58:41 +0000

    fix: update RSS feed generation to include proper types and improve site URL handling
    - Added type annotations for PostEntry and Posts.
    - Ensured the site URL defaults to "http://localhost:4321" if not provided.
    - Changed the link format from `/article/${post.id}` to `/post/${post.id}`.
    - Included a stylesheet for the RSS feed.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,23 @@
+import type { PostEntry, Posts } from "@/lib/types";
 import { getSortedPosts } from "@/lib/utils";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE } from "config";
 
-export async function GET(context: { site: any }) {
-  const posts = await getCollection("posts");
-  const sortedPosts = getSortedPosts(posts);
+export async function GET(context: { site: string }) {
+  const posts: Posts = await getCollection("posts");
+  const sortedPosts: Posts = getSortedPosts(posts);
   return rss({
     title: SITE.title,
     description: SITE.description,
-    site: context.site,
-    items: sortedPosts.map((post) => ({
+    site: context.site || "http://localhost:4321",
+    items: sortedPosts.map((post: PostEntry) => ({
       title: post.data.title,
       pubDate: post.data.pubDatetime,
       description: post.data.description,
-      link: `/article/${post.id}`,
+      link: `/post/${post.id}`,
     })),
+    stylesheet: "/rss/styles.xsl",
     customData: `<language>en-us</language>`,
   });
 }
