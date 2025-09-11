de14821b36cf6d0d6cb131a09c7c8fc0f9d978a6
Hyperoot <rajesh@hyperoot.dev>
2025-09-11 23:38:05 +0000

    feat(svg): add menu icon SVG asset

--- src/lib/initPlyr.js
+++ src/lib/initPlyr.js
@@ -20,6 +20,10 @@ export default () => {
       clickToPlay: true,
       invertTime: false,
       tooltips: { controls: true, seek: true },
+      quality: {
+        default: 1080,
+        options: [4320, 2880, 2160, 1440, 1080, 720, 576, 480, 360, 240],
+      },
     });
 
     return players;
