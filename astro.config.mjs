ea5128897b102283b3ed7badb3174ef72ef52b4a
Rajesh Das <rajesh@hyperoot.dev>
2026-02-18 21:00:26 +0000

    style(config): format astro.config.mjs for improved readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,24 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 export default defineConfig({
-    site: "https://rajeshdas.dev",
-    prefetch: true,
-    vite: {
-        plugins: [tailwindcss()],
+  site: "https://rajeshdas.dev",
+  prefetch: true,
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "css-variables",
+      defaultColor: false,
     },
-    markdown: {
-        shikiConfig: {
-            theme: "css-variables",
-            defaultColor: false,
-        },
-    },
-    integrations: [
-        icon({
-            iconDir: "src/assets/icons",
-        }),
-        AutoImport({
-            imports: ["./src/components/core/Video.astro"],
-        }),
-        mdx(),
-    ],
+  },
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+    AutoImport({
+      imports: ["./src/components/core/Video.astro"],
+    }),
+    mdx(),
+  ],
 });
